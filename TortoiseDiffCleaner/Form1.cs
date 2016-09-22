191a0a66d630acf64041d339403984203abd0a24
Kakarot <JamesRuiz@gatech.edu>
2016-09-22 18:47:25 -0400

    Allow parsing to occur on a thread to avoid hanging the UI.

--- TortoiseDiffCleaner/Form1.cs
+++ TortoiseDiffCleaner/Form1.cs
@@ -20,47 +20,58 @@ namespace TortoiseDiffCleaner
 
         private void Form1_DragDrop(object sender, DragEventArgs e)
         {
-            List<String> doubleCheck = new ArrayList().Cast<String>().ToList();
-            DeDuplicator myDeDuplicator;
 
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
                 string[] filePaths = (string[])(e.Data.GetData(DataFormats.FileDrop));
-
-                foreach (string fileLoc in filePaths)
-                {
-
-
-                    // Code to read the contents of the text file
-                    if (System.IO.File.Exists(fileLoc))
-                    {
-                        if (!(System.IO.Path.GetExtension(fileLoc) == ".patch" || System.IO.Path.GetExtension(fileLoc) == ".diff"))
-                        {
-                            MessageBox.Show(@"I only parse .diff or .patch files generated from ""Create Patch"" menu in TortoiseSVN!");
-                            break;
-                        }                        
-
-                        doubleCheck.Add(fileLoc);
-                        myDeDuplicator = new DeDuplicator(fileLoc);
-                        myDeDuplicator.Init();
-                    } //end IF for file existing
-                    //doubleCheck.ToString();
-                }// end foreach
-
-                if (doubleCheck.Count != 0)
-                {
-                    StringBuilder builder = new StringBuilder();
-                    foreach (String str in doubleCheck)
-                    {
-                        String strippedFile = System.IO.Path.GetFileName(str);
-                        builder.AppendLine(strippedFile);
-
-                    }
-                    MessageBox.Show("The following files have been converted to .patch files:\n\n" + builder.ToString()
-                        + "\n\nThese files are located in the same directory as their respective source file counterparts, which is\n\n" +
-                        System.IO.Path.GetDirectoryName(filePaths[0]));
-                } //end 'if' statment to ensure valid files were dragged to UI
+                //Make it a thread so we don't hang UI
+                Task parseFiles = new Task(() => cleanDuplicates(filePaths));
+                parseFiles.Start();
             }
+        }
+
+        private void TortoiseDiffCleaner_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        private void cleanDuplicates(string[] filePaths)
+        {
+            List<String> doubleCheck = new ArrayList().Cast<String>().ToList();
+            DeDuplicator myDeDuplicator;
+
+
+            foreach (string fileLoc in filePaths)
+            {
+                // Code to read the contents of the text file
+                if (System.IO.File.Exists(fileLoc))
+                {
+                    if (!(System.IO.Path.GetExtension(fileLoc) == ".patch" || System.IO.Path.GetExtension(fileLoc) == ".diff"))
+                    {
+                        MessageBox.Show(@"I only parse .diff or .patch files generated from ""Create Patch"" menu in TortoiseSVN!" +
+                            "\nCannot Parse: " + fileLoc);
+                        break;
+                    }
+
+                    doubleCheck.Add(fileLoc);
+                    myDeDuplicator = new DeDuplicator(fileLoc);
+                    myDeDuplicator.Init();
+                } //end IF for file existing                 
+            }// end foreach
+
+            if (doubleCheck.Count != 0)
+            {
+                StringBuilder builder = new StringBuilder();
+                foreach (String str in doubleCheck)
+                {
+                    String strippedFile = System.IO.Path.GetFileName(str);
+                    builder.AppendLine(strippedFile);
+
+                }
+                MessageBox.Show("The following files have been converted to .patch files:\n\n" + builder.ToString()
+                    + "\n\nThese files are located in the same directory as their respective source file counterparts, which is:\n\n" +
+                    System.IO.Path.GetDirectoryName(filePaths[0]), "Success!");
+            } //end 'if' statment to ensure valid files were dragged to UI
         }
     }
 }
